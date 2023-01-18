0756bce1d16ec57752b886788850be19e969c8de
rakshitha <adishesharaogan@gmail.com>
2023-01-18 18:27:03 +0530

    test

---
+++ hash.java
@@ -0,0 +1,5 @@
+Hashtable <string,string> H =new Hashtable<>();
+H.put("Mysore","karnataka");
+H.put("Hydrabad","Andra Pradesh");
+H.put("Chennai","Tamil Nadu");
+System.out.println(H);
---
+++ hw.java
@@ -0,0 +1,7 @@
+class hw{
+    pubic static void main(String args[]){
+        System.out.println("Hello world");
+    }
+
+}
+
