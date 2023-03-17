d407869817bec4235ebe293ed732e3edd322d0e9
rakshitha <adishesharaogan@gmail.com>
2023-03-17 18:20:04 +0530

    first commit

--- hash.java
+++
@@ -1,5 +0,0 @@
-Hashtable <string,string> H =new Hashtable<>();
-H.put("Mysore","karnataka");
-H.put("Hydrabad","Andra Pradesh");
-H.put("Chennai","Tamil Nadu");
-System.out.println(H);
--- hw.java
+++
@@ -1,7 +0,0 @@
-class hw{
-    pubic static void main(String args[]){
-        System.out.println("Hello world");
-    }
-
-}
-
